0d75b847f33615610acd558316b38b70f9dc175c
Matilde <152901959+matildemcr@users.noreply.github.com>
2024-12-12 10:41:17 +0000

    Update bloom_filter.m

--- BloomFilter/bloom_filter.m
+++ BloomFilter/bloom_filter.m
@@ -8,20 +8,14 @@ common_passwords = common_passwords{1};
 
 % parameters for the Bloom Filter
 n = 100000;
-k = 10; 
+k = 10;
 
 % inicialization of the Bloom Filter
 BloomFilter = init_filter(n);
 
-for i = 1:length(common_passwords)
-    
+for i = 1:length(common_passwords) 
     BloomFilter = add_element(BloomFilter, common_passwords{i}, k);
-
 end
 
-% save BloomFilter variable to a .mat file
-save('BloomFilter.mat', 'BloomFilter')
 
-% test
-bloom_filter_tests(BloomFilter, k);
-
+save('BloomFilter_model.mat', 'BloomFilter', 'n', 'k');
